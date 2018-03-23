9505f074b7ee65db5e4d280f28b9415c583a85cf
Mike Dickerson <inakajin@gmail.com>
2018-03-22 20:33:22 -0500

    for heroku deploy

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    //'url' : 'mongodb://schooladmin:schooladmin@ds123718.mlab.com:23718/fitnesstracker-db'
+    //'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://schooladmin:schooladmin@ds123718.mlab.com:23718/fitnesstracker-db'
 };
